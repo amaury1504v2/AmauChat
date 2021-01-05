710e9c068010214f3ef724a432199bf38b0e10c8
Amaury Gimonnet <amaury.gimonnet@edu.devinci.fr>
2021-01-05 23:10:56 +0100

    NotFound

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Connexion from './Components/Connexion'
+import NotFound from './Components/NotFound'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Connexion />, document.getElementById('root'))
+import { BrowserRouter, Route, Switch } from 'react-router-dom' // Imports
+
+const Root = () => (
+    <BrowserRouter>
+        <Switch> {/* Permet de prendre en enfant plusieurs routes */}
+            <Route exact path='/' component={ Connexion } />
+            <Route path='/pseudo/:pseudo' component={ App } /> {/* :/ = définir un chemin de route pas exact */}
+            <Route component={ NotFound } />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root')) 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
